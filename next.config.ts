ba112f7413220eae85bde48d3ae38efbca748c2f
Dileclo <alexa324sa@gmail.com>
2026-06-20 11:35:55 +0300

    Fix: simplify config, add explicit Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'standalone',
-  outputFileTracingIncludes: {
-    '/*': ['./hail.db', './public/**/*', './node_modules/sql.js/dist/**/*.wasm'],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
