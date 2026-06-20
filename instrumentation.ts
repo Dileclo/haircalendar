eaa9e50ca713a461779272b38909a9f07ca29359
Dileclo <alexa324sa@gmail.com>
2026-06-20 12:26:47 +0300

    Fix: re-enable notification scheduler

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,5 +2,8 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { warmup } = await import('./lib/warmup');
     await warmup();
+    const { startNotificationScheduler } = await import('./lib/notificationScheduler');
+    startNotificationScheduler();
+    console.log('[Instrumentation] Scheduler started');
   }
 }
